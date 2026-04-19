2258d67523fd7d1d115ecae71a008c79d1c0a730
Cherry <ariksab007@gmail.com>
2026-04-19 14:09:41 +0600

    Delete hokako directory changed into project-based directory

--- hokako/Word art/imagewordart.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-import pywhatkit
-
-photo = Image.open("hokako/Word art/carsLMQ.png")
-pywhatkit.image_to_ascii_art("\carsLMQ.png", "WordART")
--- hokako/Word art/read.py
+++
@@ -1,2 +0,0 @@
-read_file = open("hokako/Word art/WordART.txt","r")
-print(read_file.read())
